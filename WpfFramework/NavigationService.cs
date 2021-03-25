3737d6451e47ce54adcf3912197d3d4530f0a68e
BalintSedlak <sedlak.balint@gmail.com>
2021-03-25 13:37:23 +0100

    Add WpfFramework project

---
+++ WpfFramework/NavigationService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfFramework
+{
+    public class NavigationService : ViewModelBase
+    {
+        private Frame _frame;
+        private Page _currentContent;
+
+        public RelayCommand GoBackCommand { get; set; }
+        public RelayCommand GoForwardCommand { get; set; }
+
+        public Page CurrentContent
+        {
+            get { return _currentContent; }
+            set { SetField(ref _currentContent, value, nameof(CurrentContent)); }
+        }
+
+        public NavigationService()
+        {
+            GoBackCommand = new RelayCommand(GoBack, CanGoBack);
+            GoForwardCommand = new RelayCommand(GoForward, CanGoForward);
+        }
+
+        public void SetFrame(Frame frame)
+        {
+            if (frame is null)
+            {
+                throw new ArgumentNullException(nameof(frame));
+            }
+
+            _frame = frame;
+        }
+
+        public void NavigateTo(Page page)
+        {
+            if (page is null)
+            {
+                throw new ArgumentNullException(nameof(page));
+            }
+
+            CurrentContent = page;
+        }
+
+        public bool IsActiveContent(Page page)
+        {
+            if (!Page.Equals(_currentContent, page))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CanGoBack()
+        {
+            if (!_frame.CanGoBack)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool CanGoForward()
+        {
+            if (!_frame.CanGoForward)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void GoBack()
+        {
+            if (!_frame.CanGoBack)
+            {
+                return;
+            }
+
+            _frame.GoBack();
+        }
+
+        public void GoForward()
+        {
+            if (!_frame.CanGoForward)
+            {
+                return;
+            }
+
+            _frame.GoForward();
+        }
+    }
+}
---
+++ WpfFramework/RelayCommandGeneric.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfFramework
+{
+    public class RelayCommandGeneric<T, T1> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T1> _canExecute;
+
+        public RelayCommandGeneric(Action<T> execute, Predicate<T1> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T1)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+    }
+}
